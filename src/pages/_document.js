591752fb0046e4210c37b08c56f54001e5ff189d
hackhound2k23 <121106170+hackhound2k23@users.noreply.github.com>
2023-06-29 23:00:34 +0530

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,11 +11,11 @@ class MyDocument extends Document {
         <Head>
           <meta
             name="title"
-            content="Ogami - Multipurpose Organic Store & Bakery React Templates"
+            content="Lefin Suitcase"
           />
           <meta
             name="description"
-            content="Ogami - Multipurpose Organic Store & Bakery React Templates"
+            content="Legal and Financial Services"
           />
           <link rel="icon" href="/fav.png" />
           <link
